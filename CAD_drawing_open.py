1505d319380c348f9a42310dd42680f91e8f3ccf
conradevs <csilveiradev@gmail.com>
2022-06-01 23:53:04 -0300

    starting with convertion to grades and decimals

--- CAD_drawing_open.py
+++ CAD_drawing_open.py
@@ -1,11 +1,14 @@
 from pyautocad import Autocad, APoint, aDouble
+from comtypes import *
+from comtypes.client import CreateObject
+from comtypes import COMError
 
 acad = Autocad(create_if_not_exists=True)
 # pR = aDouble(0,0,0,2,0,0,2,4,0,0,4,0,0,0,0)
 # acad.model.AddPolyline(pR)
 print('Working at... '+acad.doc.Name)
-for obj in acad.iter_objects():
-    print (obj.ObjectName)
+#for obj in acad.iter_objects():
+#    print (obj.ObjectName)
 
 
 # print('Texts and lines')
@@ -14,21 +17,24 @@ for obj in acad.iter_objects():
 
 set = acad.get_selection(text='Select some objects')
 #for set in acad.doc.SelectionSets:
-for obj in set:
-    if(obj.ObjectName == 'AcDbPolyline'):
-        print ('there is a Polyline in the selection')
-    if(obj.ObjectName == 'AcDbLine'):
-            print ('there is a Line in the selection')
-    if(obj.ObjectName == 'AcDbCircle'):
-            print ('there is a Circle in the selection')
-            position = obj.Center
+try:
+    for obj in set:
+        if(obj.ObjectName == 'AcDbPolyline'):
+            print ('there is a Polyline in the selection')
+        if(obj.ObjectName == 'AcDbLine'):
+                print ('there is a Line in the selection')
+        if(obj.ObjectName == 'AcDbCircle'):
+                print ('there is a Circle in the selection')
+                position = obj.Center
+                print(position)
+        if(obj.ObjectName == 'AcDbBlockReference'):
+            position = obj.InsertionPoint
             print(position)
-    if(obj.ObjectName == 'AcDbBlockReference'):
-        caca=0
-        position = obj.InsertionPoint
-        print(position)
-    
-prompt1 = "Enter the start point of the line: "
+except COMError as ce:
+    target_error = ce.args # this is a tuple
+    if target_error[1] == 'Call was rejected by callee.':
+        print(target_error[1])  
+#prompt1 = "Enter the start point of the line: "
 #point_1 = APoint(0,0,1)
 #point_2 = acad.doc.Utility.GetPoint(point_1,prompt1)
 
--- Drawing1.dwl
+++
@@ -1,3 +0,0 @@
-Conrado
-DESKTOP-64N035U 
-martes, 31 de mayo de 2022  2:23:10
