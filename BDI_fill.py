3a9d2cf8a2c0c71f1629a7a24493d0c5f1bfbc4b
conradevs <csilveiradev@gmail.com>
2022-07-31 22:50:55 -0300

    UI almost finished, ACAD extraction remaining

--- BDI_fill.py
+++ BDI_fill.py
@@ -1,10 +1,11 @@
 import sys
 import copy
+from tkinter import *
 from zipfile import ZipFile
 import xml.sax, xml.sax.handler
 from kmzHandler import PlacemarkHandler
 from string_to_objects import points_lines_shapes
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
 from Line import Point
 
 BDI_CONFIGURACIONES_INIT = 9
@@ -12,10 +13,63 @@ BDI_CONFIGURACIONES_INIT = 9
 class BDI_Header:
     def __init__(self):
         self.station = ''
-        self.section = ''
+        self.circuit = ''
         self.SB_init = ''
         self.SB_end = ''
         self.work_code = ''
+    def load_from_template(self, excel_template):
+        wb = load_workbook(filename = excel_template)
+        ws = wb.active
+        self.station = ws['C4'].value
+        self.circuit = ws['M4'].value
+        self.voltage = ws['I4'].value
+        self.SB_init = ws['C6'].value
+        self.SB_end = ws['I6'].value
+        self.work_code = ws['I5'].value
+        wb.close
+    def fill_form(self,
+        work_entry: Entry,
+        station_entry: Entry,
+        circuit_entry: Entry,
+        tension_entry: Entry,
+        SB_1_entry: Entry,
+        SB_2_entry: Entry
+        ):
+        work_entry.delete(0, 'end')
+        if(self.work_code != None): work_entry.insert(0,self.work_code)
+        station_entry.delete(0, 'end')
+        if(self.station != None): station_entry.insert(0,self.station)
+        circuit_entry.delete(0, 'end')
+        if(self.circuit != None): circuit_entry.insert(0,self.circuit)
+        tension_entry.delete(0, 'end')
+        if(self.voltage != None): tension_entry.insert(0,self.voltage)
+        SB_1_entry.delete(0, 'end')
+        if(self.SB_init != None): SB_1_entry.insert(0,self.SB_init)
+        SB_2_entry.delete(0, 'end')
+        if(self.SB_end != None): SB_2_entry.insert(0,self.SB_end)
+    def get_info_from_form(self,
+        work_entry: Entry,
+        station_entry: Entry,
+        circuit_entry: Entry,
+        tension_entry: Entry,
+        SB_1_entry: Entry,
+        SB_2_entry: Entry
+        ):
+        self.station = station_entry.get()
+        self.circuit = circuit_entry.get()
+        self.voltage = tension_entry.get()
+        self.SB_init = SB_1_entry.get()
+        self.SB_end = SB_2_entry.get()
+        self.work_code = work_entry.get()
+    
+    def fill_BDI(self,wb: Workbook):
+        ws = wb.active
+        ws['C4'].value = self.station
+        ws['M4'].value = self.circuit
+        ws['I4'].value = self.voltage
+        ws['C6'].value = self.SB_init
+        ws['I6'].value = self.SB_end
+        ws['I5'].value = self.work_code
 
 class Column:
     def __init__(self,name):
@@ -139,7 +193,16 @@ def BDI_write_row(point: Column, ws, row):
        ws['Z'+str(row)].value = point.observations
        ws['AA'+str(row)].value = point.elements
 
-def test_function(file_input,directory_output):
+def test_function(header_info: BDI_Header,
+    file_input,
+    directory_output,
+    carpeta_get,
+    estacion_get,
+    salida_get,
+    tension_get,
+    SB_1_get,
+    SB_2_get
+    ):
 
     kmz = ZipFile(file_input, 'r')
     kml = kmz.open('doc.kml', 'r')
@@ -155,6 +218,8 @@ def test_function(file_input,directory_output):
     config = BDI_configurations_load()
     wb = load_workbook(filename = 'BDI_template.xlsx')
     ws = wb.active
+    header_info.get_info_from_form(header_info,carpeta_get,estacion_get,salida_get,tension_get,SB_1_get,SB_2_get)
+    header_info.fill_BDI(header_info,wb)
     postes = []
     for point in points:
         post = column_class_constructor(point,config)
@@ -163,6 +228,4 @@ def test_function(file_input,directory_output):
             postes.append(post)
     for row in range(BDI_CONFIGURACIONES_INIT,len(postes)+BDI_CONFIGURACIONES_INIT):
         BDI_write_row(postes[row-BDI_CONFIGURACIONES_INIT], ws, row)
-
-    output = points_lines_shapes(handler.mapping)
     wb.save(filename = directory_output+'/BDI_output.xlsx')
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from tkinter import *
 from tkinter import filedialog as fd
 from write_excel_cells import convert_file, bdi_ws_fill
 from CAD_drawing_open import coordinates_from_Autocad, save_in_excel_file
-from BDI_fill import test_function
+from BDI_fill import test_function, BDI_Header
 #main window
 root =Tk()
 root.title('kmz a excel')
 root.iconbitmap('Logo.ico')
-root.geometry("400x250")
+#root.geometry("400x250")
 main_labelframe = LabelFrame(root,text='Extrae coordenadas de un .kmz a un libro de excel',labelanchor='n',padx=10,pady=10)
 main_labelframe.pack(ipadx=10,ipady=10,expand=True)
 
@@ -19,36 +19,106 @@ def pick_kmz_file(label_show: Label):
     print(kmz_file)
     label_show.config(text = kmz_file)
 
-kmz_file_picker_container = LabelFrame(main_labelframe,text="Elije archivo a convertir",pady=5,width=120)
-label_file_picker = Label(kmz_file_picker_container, width=50, text = "Elegir archivo kmz...")
-button_file_picker = Button(kmz_file_picker_container,width=15,text="Buscar", command=lambda:pick_kmz_file(label_file_picker))
+kmz_file_select_container = LabelFrame(main_labelframe,text="Elije archivo a convertir",pady=5,width=120)
+label_file_select = Label(kmz_file_select_container, width=50, text = "Elegir archivo kmz...")
+button_file_select = Button(kmz_file_select_container,width=15,text="Buscar", command=lambda:pick_kmz_file(label_file_select))
 
-kmz_file_picker_container.pack(expand=True)
-label_file_picker.pack()
-button_file_picker.pack()
+kmz_file_select_container.pack(expand=True)
+label_file_select.pack()
+button_file_select.pack()
 
 # path for new directory
 def pick_directory(label_show: Label):
     path = fd.askdirectory()
     label_show.config(text = path)
 
-path_picker_container = LabelFrame(main_labelframe,text="Directorio donde guardar archivo excel",pady=5,width=120)
-label_path_picker = Label(path_picker_container, width=50, text = "Elegir directorio...")
-button_path_picker = Button(path_picker_container,width=15,text="Buscar", command=lambda:pick_directory(label_path_picker))
+path_select_container = LabelFrame(main_labelframe,text="Directorio donde guardar archivo excel",pady=5,width=120)
+label_path_select = Label(path_select_container, width=50, text = "Elegir directorio...")
+button_path_select = Button(path_select_container,width=15,text="Buscar", command=lambda:pick_directory(label_path_select))
 
-path_picker_container.pack()
-label_path_picker.pack()
-button_path_picker.pack()
+path_select_container.pack()
+label_path_select.pack()
+button_path_select.pack()
 
-print(label_file_picker.cget('text'))
-print(label_path_picker.cget('text'))
-
-result_picker_container = LabelFrame(main_labelframe,text='Elije donde quieres los resultados',pady=5,padx=5,width=120)
-result_picker_container.pack()
-button_convert = Button(result_picker_container,text="Convertir a planilla excel",command=lambda:convert_file(label_file_picker,label_path_picker))
+print(label_file_select.cget('text'))
+print(label_path_select.cget('text'))
+label_pick_output = Label(main_labelframe,text='Elige como guardar resultados')
+excel_select_container = LabelFrame(main_labelframe,text='En planilla excel',pady=5,padx=5,width=120)
+excel_select_container.pack()
+button_convert = Button(excel_select_container,text="Convertir a planilla excel",command=lambda:convert_file(label_file_select,label_path_select))
 button_convert.grid(row=0,column=0)
-button_to_BDI_file = Button(result_picker_container,text="Convertir a planilla BDI",command=lambda: test_function(label_file_picker.cget('text'),label_path_picker.cget('text')))
-button_to_BDI_file.grid(row=0,column=1)
+BDI_select_container = LabelFrame(main_labelframe,text='En planilla BDI',pady=2,padx=2)
+BDI_select_container.pack()
+# General BDI info
+    # select BDI template    
+def select_template_file(header_info: BDI_Header, show_selection: Entry):
+    excel_template = fd.askopenfile(filetypes=[('Excel Files', ['xlsx'])])
+    excel_template = excel_template.name
+    print(excel_template)
+    show_selection.delete(0, 'end')
+    show_selection.insert(0,excel_template)
+    header_info.load_from_template(header_info,excel_template)
+    header_info.fill_form(header_info,carpeta_get,estacion_get,salida_get,tension_get,SB_1_get,SB_2_get)
+
+    # button find file
+template_select = Entry(BDI_select_container)
+template_select.insert(0,'Cargar encabezado de BDI desde un template...')
+template_select.grid(row=0,columnspan=3,sticky = 'ew')
+header = BDI_Header
+button_template_select = Button(BDI_select_container,width=15,text="Buscar", command=lambda:select_template_file(header,template_select))
+button_template_select.grid(row=0,column=3)
+    
+    # fill form by hand
+carpeta_label = Label(BDI_select_container,text= 'Nro de carpeta :')
+carpeta_label.grid(row=1,column=0)
+carpeta_get = Entry(BDI_select_container)
+carpeta_get.grid(row=1,column=1,columnspan=2,sticky = 'ew')
+    # ESTACION
+estacion_label = Label(BDI_select_container,text= 'Estacion :')
+estacion_label.grid(row=2,column=0)
+estacion_get = Entry(BDI_select_container)
+estacion_get.grid(row=2,column=1)
+
+    # SALIDA
+salida_label = Label(BDI_select_container,text= 'Salida :')
+salida_label.grid(row=2,column=2)
+salida_get = Entry(BDI_select_container)
+salida_get.grid(row=2,column=3)
+
+    # Tension
+tension_label = Label(BDI_select_container,text= 'Tensión = ')
+tension_label.grid(row=3,column=0)
+tension_get = Entry(BDI_select_container)
+tension_get.grid(row=3,column=1)
+tension_V = Label(BDI_select_container,text= ' (V)')
+tension_V.grid(row=3,column=2,sticky = 'w')
+
+    # DESDE SB
+SB_1_label = Label(BDI_select_container,text= 'Desde SB :')
+SB_1_label.grid(row=4,column=0)
+SB_1_get = Entry(BDI_select_container)
+SB_1_get.grid(row=4,column=1)
+
+    # HASTA SB
+SB_2_label = Label(BDI_select_container,text= 'Hasta SB :')
+SB_2_label.grid(row=4,column=2)
+SB_2_get = Entry(BDI_select_container)
+SB_2_get.grid(row=4,column=3)
+
+
+button_to_BDI_file = Button(BDI_select_container,
+    text="Convertir a planilla BDI",
+    command=lambda: test_function(header,
+        label_file_select.cget('text'),
+        label_path_select.cget('text'),
+        carpeta_get,
+        estacion_get,
+        salida_get,
+        tension_get,
+        SB_1_get,
+        SB_2_get
+    ))
+button_to_BDI_file.grid(row=5,column=2)
 
 #def on_click_button_acad(excel_book_path):
 #    if excel_book_path == 'Elegir directorio...': 
@@ -58,7 +128,7 @@ button_to_BDI_file.grid(row=0,column=1)
 #    save_in_excel_file("ACAD_Output",excel_book_path,coord_list)
 
 
-#button_convert_from_ACAD = Button(main_labelframe,text="Extraer desde ACAD",command=lambda:on_click_button_acad(label_path_picker.cget('text')))
+#button_convert_from_ACAD = Button(main_labelframe,text="Extraer desde ACAD",command=lambda:on_click_button_acad(label_path_select.cget('text')))
 #button_convert_from_ACAD.pack(expand=True)
 root.mainloop()
 
