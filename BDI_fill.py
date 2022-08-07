5b8edec079239aa2387b35e508b533bfb70ff63b
conradevs <csilveiradev@gmail.com>
2022-08-07 15:58:28 -0300

    included BDI header options and cable options

--- BDI_fill.py
+++ BDI_fill.py
@@ -1,3 +1,4 @@
+from msilib.schema import ComboBox
 import sys
 import copy
 from tkinter import *
@@ -9,6 +10,7 @@ from openpyxl import load_workbook, Workbook
 from Line import Point
 
 BDI_CONFIGURACIONES_INIT = 9
+BDI_OPTIONS_INIT = 121
 
 class BDI_Header:
     def __init__(self):
@@ -17,6 +19,10 @@ class BDI_Header:
         self.SB_init = ''
         self.SB_end = ''
         self.work_code = ''
+        self.conductors_number = ''
+        self.line_material = ''
+        self.section = ''
+        self.cable_isulation = ''
     def load_from_template(self, excel_template):
         wb = load_workbook(filename = excel_template)
         ws = wb.active
@@ -26,6 +32,10 @@ class BDI_Header:
         self.SB_init = ws['C6'].value
         self.SB_end = ws['I6'].value
         self.work_code = ws['I5'].value
+        self.conductors_number = ws['C'+str(BDI_CONFIGURACIONES_INIT)].value
+        self.line_material = ws['D'+str(BDI_CONFIGURACIONES_INIT)].value
+        self.section = ws['E'+str(BDI_CONFIGURACIONES_INIT)].value
+        self.cable_isulation = ws['F'+str(BDI_CONFIGURACIONES_INIT)].value
         wb.close
     def fill_form(self,
         work_entry: Entry,
@@ -33,7 +43,12 @@ class BDI_Header:
         circuit_entry: Entry,
         tension_entry: Entry,
         SB_1_entry: Entry,
-        SB_2_entry: Entry
+        SB_2_entry: Entry,
+        conductors_number: ComboBox,
+        line_material: ComboBox,
+        section: ComboBox,
+        cable_isulation: ComboBox,
+        output_name: Entry
         ):
         work_entry.delete(0, 'end')
         if(self.work_code != None): work_entry.insert(0,self.work_code)
@@ -47,13 +62,28 @@ class BDI_Header:
         if(self.SB_init != None): SB_1_entry.insert(0,self.SB_init)
         SB_2_entry.delete(0, 'end')
         if(self.SB_end != None): SB_2_entry.insert(0,self.SB_end)
+        conductors_number.set('')
+        if(self.conductors_number != None): conductors_number.set(self.conductors_number)
+        line_material.set('')
+        if(self.line_material != None): line_material.set(self.line_material)
+        section.set('')
+        if(self.section != None): section.set(self.section)
+        cable_isulation.set('')
+        if(self.cable_isulation != None): cable_isulation.set(self.cable_isulation)
+        output_name.delete(0, 'end')
+        output_name.insert(0,'BDI CPT '+str(self.work_code))
+
     def get_info_from_form(self,
         work_entry: Entry,
         station_entry: Entry,
         circuit_entry: Entry,
         tension_entry: Entry,
         SB_1_entry: Entry,
-        SB_2_entry: Entry
+        SB_2_entry: Entry,
+        conductors_number: ComboBox,
+        line_material: ComboBox,
+        section: ComboBox,
+        cable_isulation: ComboBox
         ):
         self.station = station_entry.get()
         self.circuit = circuit_entry.get()
@@ -61,6 +91,10 @@ class BDI_Header:
         self.SB_init = SB_1_entry.get()
         self.SB_end = SB_2_entry.get()
         self.work_code = work_entry.get()
+        self.conductors_number = conductors_number.get()
+        self.line_material = line_material.get()
+        self.section = section.get()
+        self.cable_isulation = cable_isulation.get()
     
     def fill_BDI(self,wb: Workbook):
         ws = wb.active
@@ -105,8 +139,43 @@ class Column:
 def BDI_configurations_load():
     wb = load_workbook(filename = 'BDI_configuraciones.xlsx')
     ws = wb.active
-    row = BDI_CONFIGURACIONES_INIT
+    # Load isulation BDI options from template
+    line_isulation_options = []
+    row = BDI_OPTIONS_INIT
+    while (ws['AU'+str(row)].value != None):
+        #print(ws['A'+str(row)].value)
+        line_isulation_options.append(ws['AU'+str(row)].value)
+        row = row + 1
+    # Load number of cables BDI options from template
+    conductors_number = []
+    row = BDI_OPTIONS_INIT
+    while (ws['AX'+str(row)].value != None):
+        #print(ws['A'+str(row)].value)
+        conductors_number.append(ws['AX'+str(row)].value)
+        row = row + 1    
+    # Load conductor material BDI options from template
+    conductor_material = []
+    row = BDI_OPTIONS_INIT
+    while (ws['AY'+str(row)].value != None):
+        #print(ws['A'+str(row)].value)
+        conductor_material.append(ws['AY'+str(row)].value)
+        row = row + 1   
+    # Load conductor section BDI options from template
+    conductor_section = []
+    row = BDI_OPTIONS_INIT
+    while (ws['AZ'+str(row)].value != None):
+        #print(ws['A'+str(row)].value)
+        conductor_section.append(ws['AZ'+str(row)].value)
+        row = row + 1  
+
     output = []
+    output.append(line_isulation_options)
+    output.append(conductors_number)
+    output.append(conductor_material)
+    output.append(conductor_section)
+
+    # Load all template post configurations
+    row = BDI_CONFIGURACIONES_INIT
     while (ws['A'+str(row)].value != None):
         #print(ws['A'+str(row)].value)
         output.append(Column(ws['A'+str(row)].value))
@@ -115,17 +184,25 @@ def BDI_configurations_load():
     wb.close
     return output
 
-def column_class_constructor(point: Point,configurations):
+def post_params_fill(name, config, config_index, header: BDI_Header):
+    post = copy.deepcopy(config[config_index])
+    post.name = name
+    post.conductors = header.conductors_number
+    post.line_material = header.line_material
+    post.section = header.section
+    post.cable_isulation = header.cable_isulation
+    return post
+def column_class_constructor(point: Point,configurations, header: BDI_Header):
+    CONFIGURATIONS_START = 4
     post_config = point.name.split(' ')
     post = None
     # Apoyo suspension por defecto
     if(len(post_config) == 1):
-        post = copy.deepcopy(configurations[0]) # Cargo parametros desde template (poste C4)
-        post.name = post_config[0]
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+0,header) # Cargo parametros desde template (poste C4)
         return post
     if(post_config[1]) == 'SU':
-        post = configurations[0] # Cargo parametros desde template (poste C4)
-        if(len(post_config)>2 and post_config[2] == 'col'):
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+0,header) # Cargo parametros desde template (poste C4)
+        if(len(post_config)>2 and (post_config[2] == 'col' or post_config[2] == 'Col')):
             post.column_material = 'Columna de hormigón con orificios'
             post.strength = '3000 N (300 Kg)'
             post.cross_material = 'Metálica'
@@ -134,34 +211,55 @@ def column_class_constructor(point: Point,configurations):
             post.strength = 'Clase 5'
     # Apoyo suspension en angulo
     if(post_config[1] == 'SA' and post_config[2] == 'BA'):
-        post = copy.deepcopy(configurations[1])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+1,header)
     if(post_config[1] == 'SA' and post_config[2] == 'RS'):
-        post = copy.deepcopy(configurations[2])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+2,header)
     if(post_config[1] == 'SA' and post_config[2] == 'RD'):
-        post = copy.deepcopy(configurations[3])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+3,header)
     if(post_config[1] == 'SD'):
-        post = copy.deepcopy(configurations[4])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+4,header)
     # Apoyo amarre en angulo    
     if(post_config[1] == 'AA' and post_config[2] == 'BA'):
-        post = copy.deepcopy(configurations[5])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+5,header)
     if(post_config[1] == 'AA' and post_config[2] == 'DE'):
-        post = copy.deepcopy(configurations[6])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+6,header)
     # Apoyo amarre en angulo derivacion
     if(post_config[1] == 'AAD' and post_config[2] == 'BA'):
-        post = copy.deepcopy(configurations[7])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+7,header)
     if(post_config[1] == 'AAD' and post_config[2] == 'DE'):
-        post = copy.deepcopy(configurations[8])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+8,header)
     if(post_config[1] == 'SAD' and post_config[2] == 'BA'):
-        post = copy.deepcopy(configurations[9])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+9,header)
     if(post_config[1] == 'SAD' and post_config[2] == 'RS'):
-        post = copy.deepcopy(configurations[10])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+10,header)
     if(post_config[1] == 'SAD' and post_config[2] == 'RD'):
-        post = copy.deepcopy(configurations[11])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+11,header)
     if(post_config[1] == 'TEA'):
-        post = copy.deepcopy(configurations[12])
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+12,header)
     if(post_config[1] == 'TE'):
-        post = copy.deepcopy(configurations[13])
-    if(post != None): post.name = post_config[0]
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+13,header)
+    if('Col' in post_config or 'col' in post_config):
+        post = post_params_fill(post_config[0],configurations,CONFIGURATIONS_START+0,header)
+        post.column_material = 'Columna de hormigón con orificios'
+        post.strength = '3000 N (300 Kg)'
+        post.cross_material = 'Metálica'
+        post.earth = 'Si'
+        if('12/500' in post_config):
+            post.column_height = '12'
+            post.strength = '5000 N (500 Kg)'
+        if('12/800' in post_config):
+            post.column_height = '12'
+            post.strength = '8000 N (800 Kg)'
+            post.rein = 'Sin rienda'
+        if('12/1200' in post_config):
+            post.column_height = '12'
+            post.strength = '12000 N (1200 Kg)'
+            post.rein = 'Sin rienda'
+        if('12/2000' in post_config):
+            post.column_height = '12'
+            post.strength = '20000 N (2000 Kg)'
+            post.rein = 'Sin rienda'
+
     return post
 
 def BDI_write_row(point: Column, ws, row):
@@ -193,18 +291,25 @@ def BDI_write_row(point: Column, ws, row):
        ws['Z'+str(row)].value = point.observations
        ws['AA'+str(row)].value = point.elements
 
-def test_function(header_info: BDI_Header,
-    file_input,
+def test_function(output_name,file_type,header_info: BDI_Header,
+    config,
+    kmz_file_input,
     directory_output,
     carpeta_get,
     estacion_get,
     salida_get,
     tension_get,
     SB_1_get,
-    SB_2_get
+    SB_2_get,
+    conductors_number_CBox,
+    line_material_CBox,
+    section_CBox,
+    cable_isulation_CBox
     ):
 
-    kmz = ZipFile(file_input, 'r')
+    print ('Output file name : '+output_name)
+
+    kmz = ZipFile(kmz_file_input, 'r')
     kml = kmz.open('doc.kml', 'r')
 
     parser = xml.sax.make_parser()
@@ -215,17 +320,27 @@ def test_function(header_info: BDI_Header,
 
     points = points_lines_shapes(handler.mapping)[0]
 
-    config = BDI_configurations_load()
     wb = load_workbook(filename = 'BDI_template.xlsx')
     ws = wb.active
-    header_info.get_info_from_form(header_info,carpeta_get,estacion_get,salida_get,tension_get,SB_1_get,SB_2_get)
+    header_info.get_info_from_form(header_info,carpeta_get,
+        estacion_get,
+        salida_get,
+        tension_get,
+        SB_1_get,
+        SB_2_get,
+        conductors_number_CBox,
+        line_material_CBox,
+        section_CBox,
+        cable_isulation_CBox
+    )
     header_info.fill_BDI(header_info,wb)
     postes = []
     for point in points:
-        post = column_class_constructor(point,config)
+        post = column_class_constructor(point,config,header_info)
         if(post != None):
             post.coordinates = "S" + point.coord_y[1:10] + "° " + "W"+point.coord_x[1:10] + "°"
             postes.append(post)
     for row in range(BDI_CONFIGURACIONES_INIT,len(postes)+BDI_CONFIGURACIONES_INIT):
         BDI_write_row(postes[row-BDI_CONFIGURACIONES_INIT], ws, row)
-    wb.save(filename = directory_output+'/BDI_output.xlsx')
+    if (file_type==None or file_type==''): file_type = '.xlsx'
+    wb.save(filename = directory_output+'/'+output_name+file_type)
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter.ttk import Combobox
 from write_excel_cells import convert_file, bdi_ws_fill
 from CAD_drawing_open import coordinates_from_Autocad, save_in_excel_file
-from BDI_fill import test_function, BDI_Header
+from BDI_fill import test_function, BDI_Header, BDI_configurations_load
+# Load BDI configurations from template
+Config = BDI_configurations_load()
 #main window
 root =Tk()
 root.title('kmz a excel')
@@ -47,9 +50,12 @@ excel_select_container = LabelFrame(main_labelframe,text='En planilla excel',pad
 excel_select_container.pack()
 button_convert = Button(excel_select_container,text="Convertir a planilla excel",command=lambda:convert_file(label_file_select,label_path_select))
 button_convert.grid(row=0,column=0)
+# BDI Heder info container
 BDI_select_container = LabelFrame(main_labelframe,text='En planilla BDI',pady=2,padx=2)
 BDI_select_container.pack()
 # General BDI info
+BDI_Header_container = LabelFrame(BDI_select_container)
+BDI_Header_container.grid(row=0,column=0)
     # select BDI template    
 def select_template_file(header_info: BDI_Header, show_selection: Entry):
     excel_template = fd.askopenfile(filetypes=[('Excel Files', ['xlsx'])])
@@ -58,57 +64,116 @@ def select_template_file(header_info: BDI_Header, show_selection: Entry):
     show_selection.delete(0, 'end')
     show_selection.insert(0,excel_template)
     header_info.load_from_template(header_info,excel_template)
-    header_info.fill_form(header_info,carpeta_get,estacion_get,salida_get,tension_get,SB_1_get,SB_2_get)
+    header_info.fill_form(header_info,
+        carpeta_get,
+        estacion_get,
+        salida_get,
+        tension_get,
+        SB_1_get,
+        SB_2_get,
+        Fase_number_get,
+        Material_get,
+        Seccion_get,
+        Aislacion_get,
+        output_file_name_get
+    )
 
     # button find file
-template_select = Entry(BDI_select_container)
+template_select = Entry(BDI_Header_container)
 template_select.insert(0,'Cargar encabezado de BDI desde un template...')
 template_select.grid(row=0,columnspan=3,sticky = 'ew')
 header = BDI_Header
-button_template_select = Button(BDI_select_container,width=15,text="Buscar", command=lambda:select_template_file(header,template_select))
+button_template_select = Button(BDI_Header_container,width=15,text="Buscar", command=lambda:select_template_file(header,template_select))
 button_template_select.grid(row=0,column=3)
     
     # fill form by hand
-carpeta_label = Label(BDI_select_container,text= 'Nro de carpeta :')
+carpeta_label = Label(BDI_Header_container,text= 'Nro de carpeta :')
 carpeta_label.grid(row=1,column=0)
-carpeta_get = Entry(BDI_select_container)
+carpeta_get = Entry(BDI_Header_container)
 carpeta_get.grid(row=1,column=1,columnspan=2,sticky = 'ew')
     # ESTACION
-estacion_label = Label(BDI_select_container,text= 'Estacion :')
+estacion_label = Label(BDI_Header_container,text= 'Estacion :')
 estacion_label.grid(row=2,column=0)
-estacion_get = Entry(BDI_select_container)
+estacion_get = Entry(BDI_Header_container)
 estacion_get.grid(row=2,column=1)
 
     # SALIDA
-salida_label = Label(BDI_select_container,text= 'Salida :')
+salida_label = Label(BDI_Header_container,text= 'Salida :')
 salida_label.grid(row=2,column=2)
-salida_get = Entry(BDI_select_container)
+salida_get = Entry(BDI_Header_container)
 salida_get.grid(row=2,column=3)
 
     # Tension
-tension_label = Label(BDI_select_container,text= 'Tensión = ')
+tension_label = Label(BDI_Header_container,text= 'Tensión = ')
 tension_label.grid(row=3,column=0)
-tension_get = Entry(BDI_select_container)
+tension_get = Entry(BDI_Header_container)
 tension_get.grid(row=3,column=1)
-tension_V = Label(BDI_select_container,text= ' (V)')
+tension_V = Label(BDI_Header_container,text= ' (V)')
 tension_V.grid(row=3,column=2,sticky = 'w')
 
     # DESDE SB
-SB_1_label = Label(BDI_select_container,text= 'Desde SB :')
+SB_1_label = Label(BDI_Header_container,text= 'Desde SB :')
 SB_1_label.grid(row=4,column=0)
-SB_1_get = Entry(BDI_select_container)
+SB_1_get = Entry(BDI_Header_container)
 SB_1_get.grid(row=4,column=1)
 
     # HASTA SB
-SB_2_label = Label(BDI_select_container,text= 'Hasta SB :')
+SB_2_label = Label(BDI_Header_container,text= 'Hasta SB :')
 SB_2_label.grid(row=4,column=2)
-SB_2_get = Entry(BDI_select_container)
+SB_2_get = Entry(BDI_Header_container)
 SB_2_get.grid(row=4,column=3)
 
+    # Cable and Output Name container
+Cable_info_container = LabelFrame(BDI_select_container)
+Cable_info_container.grid(row=1,column=0)
+    # Cable properties
+Fase_number_label = Label(Cable_info_container,text= 'N° de conductores :')
+Fase_number_label.grid(row=0,column=0)
+Fase_number_get = Combobox(Cable_info_container)
+Fase_number_get['values'] = Config[1]
+Fase_number_get.grid(row=1,column=0)
+    # Conductor material
+Material_label = Label(Cable_info_container,text= 'Material del cable :')
+Material_label.grid(row=0,column=1)
+Material_get = Combobox(Cable_info_container)
+Material_get['values'] = Config[2]
+Material_get.grid(row=1,column=1)
+    # Cable properties
+Seccion_label = Label(Cable_info_container,text= 'Seccion :')
+Seccion_label.grid(row=0,column=2)
+Seccion_get = Combobox(Cable_info_container)
+Seccion_get['values'] = Config[3]
+Seccion_get.grid(row=1,column=2)
+    # Conductor material
+Aislacion_label = Label(Cable_info_container,text= 'Aislacion conductor :')
+Aislacion_label.grid(row=0,column=3)
+Aislacion_get = Combobox(Cable_info_container)
+Aislacion_get['values'] = Config[0]
+Aislacion_get.grid(row=1,column=3)
+
+    # Output frame
+output_labelframe = LabelFrame(BDI_select_container)
+output_labelframe.grid(row=2,column=0,sticky='we')
+    # Output BDI file name
+output_file_name_label = Label(output_labelframe, text = 'Nombre del archivo BDI')
+output_file_name_label.grid(row=0,columnspan=3,ipadx=70,sticky='we')
+output_file_name_get = Entry(output_labelframe)
+output_file_name_get.insert(0,'Escribe un nombre para la nueva BDI...')
+output_file_name_get.grid(row=1,columnspan=3,ipadx=70,sticky='we')
+    # Output BDI file extension (.xlsx or .xls)
+extension_label = Label(output_labelframe,text= 'Extension (.xlsx o .xls)')
+extension_label.grid(row=0,column=3,ipadx=20,sticky='e')
+extension_get = Combobox(output_labelframe)
+extension_get['values'] = ['.xlsx','.xls']
+extension_get.set('.xlsx')
+extension_get.grid(row=1,column=3,ipadx=20,sticky='e')
 
 button_to_BDI_file = Button(BDI_select_container,
     text="Convertir a planilla BDI",
-    command=lambda: test_function(header,
+    command=lambda: test_function(
+        output_file_name_get.get(),
+        extension_get.get(),
+        header, Config,
         label_file_select.cget('text'),
         label_path_select.cget('text'),
         carpeta_get,
@@ -116,9 +181,13 @@ button_to_BDI_file = Button(BDI_select_container,
         salida_get,
         tension_get,
         SB_1_get,
-        SB_2_get
+        SB_2_get,
+        Fase_number_get,
+        Material_get,
+        Seccion_get,
+        Aislacion_get
     ))
-button_to_BDI_file.grid(row=5,column=2)
+button_to_BDI_file.grid(row=3,column=0)
 
 #def on_click_button_acad(excel_book_path):
 #    if excel_book_path == 'Elegir directorio...': 
